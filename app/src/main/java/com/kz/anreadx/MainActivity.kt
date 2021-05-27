544d01c87df255a45dd190ff6b29e4fbc605e649
X <664851432@qq.com>
2021-05-27 22:32:29 +0800

    replace root di from androidXModule to androidCoreModule

--- app/src/main/java/com/kz/anreadx/MainActivity.kt
+++ app/src/main/java/com/kz/anreadx/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.kz.anreadx.ui.Router
+import org.kodein.di.android.androidCoreModule
 import org.kodein.di.android.x.androidXModule
 import org.kodein.di.compose.withDI
 
@@ -11,7 +12,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            withDI(androidXModule(application)) {
+            withDI(androidCoreModule(application)) {
                 Router()
             }
         }
