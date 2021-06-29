306324e8e4e458df79f354dc6f0db8f22c2c2cbc
kz <kzaemrio@protonmail.com>
2021-06-29 14:28:20 +0800

    add comment for MutableStateFlow.reduce

--- app/src/main/java/com/kz/anreadx/ktx/StateFlowKtx.kt
+++ app/src/main/java/com/kz/anreadx/ktx/StateFlowKtx.kt
@@ -2,6 +2,13 @@ package com.kz.anreadx.ktx
 
 import kotlinx.coroutines.flow.MutableStateFlow
 
-inline fun <T> MutableStateFlow<T>.reduce(transform: T.() -> T) {
+/**
+ * data class TodoItem(val text: String, val done: Boolean)
+ * val flow = MutableStateFlow(TodoItem("study", false))
+ * flow.reduce { copy(done = true) } // flow.value = flow.value.copy(done = true)
+ */
+inline fun <T> MutableStateFlow<T>.reduce(
+    transform: T.() -> T
+) {
     value = value.transform()
 }
