f3bbbac0673239f9f1beec2ae93333bf2d5650ba
kz <kzaemrio@protonmail.com>
2021-09-08 11:55:23 +0800

    use com.github.kzaemrio.FlowStore

--- app/src/main/java/com/kz/anreadx/ui/FeedListViewModel.kt
+++ app/src/main/java/com/kz/anreadx/ui/FeedListViewModel.kt
@@ -4,13 +4,11 @@ import androidx.lifecycle.ViewModel
 import com.kz.anreadx.dispatcher.CPU
 import com.kz.anreadx.ktx.launch
 import com.kz.anreadx.ktx.map
-import com.kz.anreadx.ktx.reduce
 import com.kz.anreadx.model.Feed
 import com.kz.anreadx.repository.FeedListRepository
-import kotlinx.coroutines.flow.MutableStateFlow
+import com.kz.anreadx.ui.UiStateStore.Companion.asStore
+import com.kz.flowstore.annotation.FlowStore
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.withContext
 
 class FeedListViewModel constructor(
@@ -18,56 +16,50 @@ class FeedListViewModel constructor(
     private val repository: FeedListRepository
 ) : ViewModel() {
 
-    private val isRefreshing = MutableStateFlow(false)
-
-    private val list = MutableStateFlow(emptyList<FeedItem>())
-
-    private val errorMessage = MutableStateFlow(NO_ERROR)
-
-    private val _uiStateFlow: MutableStateFlow<UiState> = MutableStateFlow(UiState())
+    private val store = UiState().asStore()
 
     val uiStateFlow: StateFlow<UiState>
-        get() = _uiStateFlow
+        get() = store.flow
 
     init {
-        launch {
-            combine(isRefreshing, list, errorMessage, ::UiState).collect {
-                _uiStateFlow.emit(it)
-            }
-        }
         updateList()
     }
 
     fun updateList() {
         launch {
-            isRefreshing.reduce { true }
-            errorMessage.reduce { NO_ERROR }
+            store.isRefreshing { true }
+            store.errorMessage { NO_ERROR }
             try {
                 repository.update()
             } catch (e: Exception) {
-                errorMessage.reduce { e.message ?: NO_ERROR }
+                store.errorMessage { e.message ?: NO_ERROR }
             }
             val feedList: List<Feed> = repository.getList()
             val feedItemList = withContext(cpu) { feedList.map { FeedItem(feed = this) } }
-            list.reduce { feedItemList }
-            isRefreshing.reduce { false }
+            store.list { feedItemList }
+            store.isRefreshing { false }
         }
     }
 
     fun clearAll() {
         launch { repository.readAll() }
 
-        isRefreshing.reduce { true }
-        list.reduce { map { copy(done = true) } }
-        isRefreshing.reduce { false }
+        launch {
+            store.isRefreshing { true }
+            store.list { map { copy(done = true) } }
+            store.isRefreshing { false }
+        }
     }
 
     fun read(item: FeedItem) {
         launch { repository.read(item.id) }
-        list.reduce { map { copy(done = if (id == item.id) true else done) } }
+        launch {
+            store.list { map { copy(done = if (id == item.id) true else done) } }
+        }
     }
 }
 
+@FlowStore
 data class UiState(
     val isRefreshing: Boolean = false,
     val list: List<FeedItem> = emptyList(),
