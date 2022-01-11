ede953ef81f04d150cb14a9b7d884c5495767ccf
kz <kzaemrio@protonmail.com>
2022-01-11 18:52:03 +0800

    rm background dispatcher in respository

--- app/src/main/java/com/kz/anreadx/repository/FeedListRepository.kt
+++ app/src/main/java/com/kz/anreadx/repository/FeedListRepository.kt
@@ -7,12 +7,11 @@ import com.kz.anreadx.network.RssService
 import com.kz.anreadx.persistence.FeedDao
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 
 class FeedListRepository @Inject constructor(
-    private val background: Background,
+    background: Background,
     private val feedDao: FeedDao,
     private val rssService: RssService
 ) {
@@ -25,28 +24,18 @@ class FeedListRepository @Inject constructor(
         }
     }
 
-    suspend fun localList(): List<Feed> = withContext(background) {
-        feedDao.list()
-    }
+    suspend fun localList(): List<Feed> = feedDao.list()
 
-    suspend fun refresh(): Unit = withContext(background) {
-        feedDao.insert(requestList())
-    }
+    suspend fun refresh(): Unit = feedDao.insert(requestList())
 
-    suspend fun readAll(): Unit = withContext(background) {
-        feedDao.readAll()
-    }
+    suspend fun readAll(): Unit = feedDao.readAll()
 
-    suspend fun read(id: String): Unit = withContext(background) {
-        feedDao.read(id)
-    }
+    suspend fun read(id: String): Unit = feedDao.read(id)
 
-    private suspend fun requestList(): List<Feed> = withContext(background) {
-        when (val response = rssService.rss()) {
-            is NetworkResponse.Success -> response.body.channel.feedList
-            is NetworkResponse.ServerError -> throw response.error
-            is NetworkResponse.NetworkError -> throw response.error
-            is NetworkResponse.UnknownError -> throw response.error
-        }
+    private suspend fun requestList(): List<Feed> = when (val response = rssService.rss()) {
+        is NetworkResponse.Success -> response.body.channel.feedList
+        is NetworkResponse.ServerError -> throw response.error
+        is NetworkResponse.NetworkError -> throw response.error
+        is NetworkResponse.UnknownError -> throw response.error
     }
 }
--- app/src/main/java/com/kz/anreadx/repository/LastPositionRepository.kt
+++ app/src/main/java/com/kz/anreadx/repository/LastPositionRepository.kt
@@ -1,24 +1,17 @@
 package com.kz.anreadx.repository
 
-import com.kz.anreadx.dispatcher.Background
 import com.kz.anreadx.model.LastPosition
 import com.kz.anreadx.persistence.LastPositionDao
-import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class LastPositionRepository @Inject constructor(
-    private val background: Background,
     private val dao: LastPositionDao
 ) {
     suspend fun insert(link: String, offset: Int) {
-        withContext(background) {
-            dao.insert(LastPosition(link, offset))
-        }
+        dao.insert(LastPosition(link, offset))
     }
 
     suspend fun query(): LastPosition? {
-        return withContext(background) {
-            dao.query()
-        }
+        return dao.query()
     }
 }
