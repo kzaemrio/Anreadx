09fef5dbe6f811fa01eb44fdae08ac231789bdb4
kz <kzaemrio@protonmail.com>
2022-01-13 13:31:38 +0800

    rm init coroutine in app

--- app/src/main/java/com/kz/anreadx/App.kt
+++ app/src/main/java/com/kz/anreadx/App.kt
@@ -4,8 +4,6 @@ import android.app.Application
 import com.kz.anreadx.di.ExecutorEntryPoint
 import dagger.hilt.android.EntryPointAccessors
 import dagger.hilt.android.HiltAndroidApp
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
 
 lateinit var coil_image_path: String
 
@@ -15,14 +13,6 @@ class App : Application() {
         super.onCreate()
 
         EntryPointAccessors.fromApplication<ExecutorEntryPoint>(this).executor().execute {
-            Dispatchers.Main.apply {
-                // MainDispatcherLoader.loadMainDispatcher()
-            }
-
-            Job.apply {
-                // initializeDefaultExceptionHandlers()
-            }
-
             coil_image_path = cacheDir.apply { mkdirs() }.resolve("coil_image_cache").absolutePath
         }
     }
